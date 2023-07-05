614ea8bf5b4c6afa2328eb3f1574cc38ba5814c5
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-05 08:52:45 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () =>{
+  return (
+    <section className="App">
+        <h1 className="text-blue-500">hello world</h1>
+    </section>
+  )
+}
+export default App
