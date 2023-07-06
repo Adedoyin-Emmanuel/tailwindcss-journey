b91546f2344d237fcc336c90b00727016f5a2e76
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-06 05:38:29 +0100

    Learnt the tailwind utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-const App = () =>{
+const App = () => {
   return (
     <section className="App">
-        <h1 className="text-blue-500">hello world</h1>
+      <h1 className="text-blue-500">hello world</h1>
+      <div className="text-5xl font-extrabold ...">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
+          Hello world
+        </span>
+      </div>
     </section>
-  )
-}
-export default App
+  );
+};
+export default App;
