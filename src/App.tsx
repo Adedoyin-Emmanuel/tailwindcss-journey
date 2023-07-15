fd26d77f1a49ec3403f39fa0f30f1a49f9a145d2
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-07-16 00:47:35 +0100

    Bulding components to test my tailwind skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import LinuxLogo from "./assets/linux-logo.png";
 import "./App.css";
 
 const App = () => {
   return (
-    <section className="App">
-      <h1 className="text-blue-500">hello world</h1>
-      <div className="text-5xl font-extrabold ...">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
-          Hello world
-        </span>
-      </div>
+    <section className="App w-full h-screen bg-gradient-to-bl from-black to-blue-950  ">
+      <section className="w-full flex items-center justify-center flex-col ">
+        <h1 className="text-3xl md:text-4xl  capitalize bg-clip-text text-transparent bg-gradient-to-l from-sky-500 to-slate-100 font-bold p-5 text-center md:p-4 my-5">
+          revolutionzing shopping exprience using blockchain
+        </h1>
+        
+        <section className="my-5">
+          <img src={LinuxLogo} alt="linux logo" />
+        </section>
+      </section>
     </section>
   );
 };
